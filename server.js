e89e1e30656d9e123aa781f52e0224c9e323c65f
Agustin Ardizzone <ardizzone.agustin@gmail.com>
2020-12-15 17:30:03 -0300

    implementado /api/whoami

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function(req,res){
+  
+  res.json({ipadress: req.ip,language: req.acceptsLanguages(),software:req.headers["user-agent"]});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
